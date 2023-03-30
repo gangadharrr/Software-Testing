4632095cb848c30eec4ecca3c53b2a7fe54ecd9f
Gangadhar C <gangadharganga90@gmail.com>
2023-03-30 13:17:08 +0530

    initial commit

---
+++ Software Testing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Software_Testing
+{
+    
+    public class Program
+    {
+        
+        public static int Factorial(int N)
+        {
+
+            if(N == 0 || N==1) return 1;
+            return N*Factorial(N - 1);
+
+        }
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, World!");
+           // throw new Exception("Exception in error");
+        }
+
+    }
+}
---
+++ Software TestingTests/ProgramTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Software_Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Software_Testing.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        public static FileStream file = new FileStream("D:/TestCases.txt", FileMode.Open, FileAccess.Read);
+        public static StreamReader sr = new StreamReader(file);
+        [TestMethod()]
+        public void FactorialTest()
+        {
+
+            string outp = Program.Factorial(Convert.ToInt32(sr.ReadLine())).ToString();
+            Assert.AreEqual(sr.ReadLine(), outp);
+
+        } [TestMethod()]
+        public void FactorialTest1()
+        {
+
+            string outp = Program.Factorial(Convert.ToInt32(sr.ReadLine())).ToString();
+            Assert.AreEqual(sr.ReadLine(), outp);
+
+        }
+        [TestMethod()]
+        public void FactorialTest2()
+        {
+
+            string outp = Program.Factorial(Convert.ToInt32(sr.ReadLine())).ToString();
+            Assert.AreEqual(sr.ReadLine(), outp);
+            sr.Close();
+            file.Close();
+        }
+        [TestMethod()]
+        public void MainTest()
+        {
+            try 
+            {
+                Program.Main(new string[] { "hi"});
+                Assert.IsTrue(true);
+
+            }
+            catch (Exception ex) 
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
